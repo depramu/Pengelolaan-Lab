803559e4e737511b0b2778914e1f0c1eca4770a3
depramu <dimaspramudya1101@gmail.com>
2025-05-27 15:55:15 +0700

    folder

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+    <?php $serverName = "DEPENIGER\\SQLEXPRESS01";
+    $connectionOptions = ["Database" => "Pengelolaan_LAB", "TrustServerCertificate" => true,];
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    if ($conn === false) {
+        echo "Koneksi Gagal:<br>";
+        die(print_r(sqlsrv_errors(), true));
+    } else {
+        echo "Koneksi Berhasil!";
+    }
