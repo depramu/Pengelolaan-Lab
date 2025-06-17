e62707e8801a57e85c0177d8c92a8af9630d996b
kesyanoventa <noventakesya@gmail.com>
2025-06-17 13:26:32 +0700

    mau liat nadira

--- CRUD/Ruangan/tambahRuangan.php
+++ CRUD/Ruangan/tambahRuangan.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $cekNamaRow = sqlsrv_fetch_array($cekNamaStmt, SQLSRV_FETCH_ASSOC);
 
     if ($cekNamaRow['jumlah'] > 0) {
-        $error = "Nama ruangan sudah terdaftar, silakan gunakan nama lain.";
+        $error = "Nama ruangan sudah terdaftar";
     } else {
         $query = "INSERT INTO Ruangan (idRuangan, namaRuangan, kondisiRuangan, ketersediaan) VALUES (?, ?, ?, ?)";
         $params = [$idRuangan, $namaRuangan, $kondisiRuangan, $ketersediaan];
@@ -73,27 +73,27 @@ include '../../templates/sidebar.php';
                     <div class="card-body">
                         <form method="POST">
                             <div class="mb-2">
-                                <label for="idRuangan" class="form-label">ID Ruangan</label>
+                                <label for="idRuangan" class="form-label d-flex align-items-center">ID Ruangan</label>
                                 <input type="text" class="form-control" id="idRuangan" name="idRuangan" value="<?= htmlspecialchars($idRuangan) ?>" disabled>
                             </div>
                             <div class="mb-2">
-                                <label for="namaRuangan" class="form-label">Nama Ruangan
+                                <label for="namaRuangan" class="form-label d-flex align-items-center">Nama Ruangan
                                     <span id="namaError" class="text-danger ms-2" style="font-size:0.95em;display:none;">*Harus Diisi</span>
                                 </label>
-                                <input type="text" class="form-control" id="namaRuangan" name="namaRuangan">
+                                <input type="text" class="form-control" id="namaRuangan" name="namaRuangan" value="<?= isset($namaRuangan) ? htmlspecialchars($namaRuangan) : '' ?>">
                             </div>
                             <div class="mb-2">
-                                <label for="kondisiRuangan" class="form-label">Kondisi Ruangan
+                                <label for="kondisiRuangan" class="form-label d-flex align-items-center">Kondisi Ruangan
                                     <span id="kondisiError" class="text-danger ms-2" style="display:none;font-size:0.95em;">*Harus diisi</span>
                                 </label>
-                                <select class="form-select" id="kondisiRuangan" name="kondisiRuangan">
+                                <select class="form-select" id="kondisiRuangan" name="kondisiRuangan" value="<?= isset($kondisiRuangan) ? htmlspecialchars($kondisiRuangan) : '' ?>">
                                     <option disabled selected>Pilih Kondisi</option>
-                                    <option value="Baik">Baik</option>
-                                    <option value="Rusak">Rusak</option>
+                                    <option value="Baik" <?= (isset($kondisiRuangan) && $kondisiRuangan === 'Baik') ? 'selected' : '' ?>>Baik</option>
+                                    <option value="Rusak" <?= (isset($kondisiRuangan) && $kondisiRuangan === 'Rusak') ? 'selected' : '' ?>>Rusak</option>
                                 </select>
                             </div>
                             <div class="mb-2">
-                                <label for="ketersediaan" class="form-label">Ketersediaan Ruangan
+                                <label for="ketersediaan" class="form-label d-flex align-items-center">Ketersediaan Ruangan
                                     <span id="ketersediaanError" class="text-danger ms-2" style="display:none;font-size:0.95em;">*Harus diisi</span>
                                 </label>
                                 <select class="form-select" id="ketersediaan" name="ketersediaan">
